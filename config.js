1bddb093b0e21e293b07ebe6984732c3691316c0
Sugandhraj Patel <psugandhraj@gmail.com>
2021-11-18 12:10:27 +0530

    taking redirect uri directly from href wont work. Need to change https to http

--- config.js
+++ config.js
@@ -5,11 +5,13 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 const config = {
   client_id: process.env.CLIENT_ID,
+  redirect_uri: process.env.REDIRECT_URI,
   client_secret: process.env.CLIENT_SECRET,
 };
 
 const envVarsSchema = Joi.object({
   client_id: Joi.string().required(),
+  redirect_uri: Joi.string().required(),
   client_secret: Joi.string().required(),
 });
 
